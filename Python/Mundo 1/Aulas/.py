13a84cd408778e9c1bfc980e5c1ca6aeed39f907
guilherme <guisantogues@gmail.com>
2022-03-20 22:41:11 -0300

    exemplos aula

---
+++ Python/Mundo 1/Aulas/.py
@@ -0,0 +1,4 @@
+from math import trunc
+num = float(input('Digite um número real: '))
+ni = trunc (num)
+print('Seu número {} tem a porção inteira de {} !'.format(num, ni))
--- Python/Mundo 1/Aulas/aula.modulos.py
+++ Python/Mundo 1/Aulas/aula.modulos.py
@@ -1,4 +1,4 @@
 from math import ceil, sqrt
-num = int(input('Digite um número: ')) 
+num = int(input('Digite um número: '))
 raiz =  sqrt (num)
 print ('A raiz do numero {} é igual a {:.2f}'.format(num, ceil(raiz)))
