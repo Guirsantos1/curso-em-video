9b9030f612f3fcc52e914d605fcc2d6e0ded7cc3
Guilherme Rodrigues dos Santos <guisantogues@gmail.com>
2022-05-13 15:34:58 -0300

    Create ex045.py

---
+++ Python/Mundo 1/Exercícios/ex045.py
@@ -0,0 +1,36 @@
+from random import randint
+lista = ('Pedra','Papel','Tesoura')
+maquina = randint (0,2)
+print('''Suas Opções:
+[0] Pedra
+[1] Papel
+[2] Tesoura''')
+jogador = int(input('Qual sua jogada ? '))
+print(f'O Computador jogou: {lista[maquina]}')
+print(f'O Jogador jogou: {lista[jogador]}')
+if jogador == maquina:
+    print('EMPATE')
+#MAQUINA JOGA PEDRA
+elif maquina == 0:
+    if jogador == 1:
+        print('VOCÊ GANHOU')
+    elif jogador == 2:
+        print('VOCÊ PERDEU')
+    else:
+        print('Jogada invalida')
+#MAQUINA JOGA PAPEL
+elif maquina == 1:
+    if jogador == 0:
+        print('VOCÊ PERDEU')
+    elif jogador == 2: 
+        print('VOCÊ GANHOU')
+    else:
+        print('Jogada invalida')
+#MAQUINA JOGA TESOURA        
+elif maquina == 2:
+    if jogador == 0:
+        print('VOCÊ GANHOU')
+    elif jogador == 1:
+        print('VOCÊ PERDEU')
+    else:
+        print('Jogada invalida')
