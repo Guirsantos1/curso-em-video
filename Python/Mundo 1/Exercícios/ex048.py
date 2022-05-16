88e6f31a1ae417a9cf53c66077d5979641bb0819
Guilherme Rodrigues dos Santos <guisantogues@gmail.com>
2022-05-16 17:49:37 -0300

    exercicio

--- Python/Mundo 1/Exercícios/ex048.py
+++ Python/Mundo 1/Exercícios/ex048.py
@@ -2,6 +2,6 @@ soma = 0
 cont = 0
 for c in range (1, 501, 2):
     if c % 3 == 0:
-        soma = soma + c
-        cont = cont + 1
-print (f'A soma de {cont} números é {soma}')
+        soma += c
+        cont +=  1
+print (f'A soma dos {cont} valores é {soma}')
