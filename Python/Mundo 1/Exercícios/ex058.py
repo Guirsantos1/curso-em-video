ac82550673cd0a2e79e06532ec03172a50cf5eb1
Guilherme Rodrigues dos Santos <guisantogues@gmail.com>
2022-05-23 20:51:49 -0300

    att

--- Python/Mundo 1/Exercícios/ex058.py
+++ Python/Mundo 1/Exercícios/ex058.py
@@ -2,8 +2,9 @@ from random import randint
 maquina = randint (0,10)
 print('Pensei em um número entre 0 e 10.')
 print('Tente acertar!')
-palpites = 1
-while palpites != maquina:
+palpites = 0
+jogador = 0
+while jogador != maquina:
     jogador = int(input('Qual sua respota?  '))
     palpites +=1
 print(f'''Parabéns o numero pensado foi {maquina}, !
