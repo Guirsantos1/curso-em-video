afde99315f549f506453354e7602569eeba1af0a
Guilherme Rodrigues dos Santos <guisantogues@gmail.com>
2022-05-28 22:49:03 -0300

    Update ex070.py

--- Python/Mundo 1/Exercícios/ex070.py
+++ Python/Mundo 1/Exercícios/ex070.py
@@ -1,19 +1,23 @@
 print(20*'-')
 print(4*' ','LOJAS GUI')
 print(20*'-')
-soma = prodmil = prodbarato = cont = 0
+soma = prodmil = cont = menor =  0
+barato = ' '
 while True:
     prod = str(input('Nome do produto:' )).strip().title()
     prec = float(input('Preço: '))
     soma += prec
-    cont += 1 
+    cont += 1
+    if cont == 1 or prec < menor:
+        menor = prec
+        barato = prod
     if prec > 1000:
         prodmil +=1
     resp = ' '
-    while resp not in 'SN':
+    while resp not in 'SN': #Pode ser tbm while resp !='S' and resp != 'N'
         resp = str(input('Quer continuar comprando [S/N]? ' )).strip().upper()[0]
     if resp == 'N':
         break
 print(10*'-','FIM DO PROGRAMA',10*'-')
 print(f'O total da compra foi R${soma:.2f}\n{prodmil} Produtos com valor acima de R$1000.00')
-print(f'O produto mais barato foi {prodbarato} no valor de R$ ')
+print(f'O produto mais barato foi {barato} no valor de R$ {menor}')
