0704b8037c103b2ec54dd03745791108e85ed6f9
Guilherme Rodrigues dos Santos <guisantogues@gmail.com>
2023-01-26 23:29:52 -0300

    estudos

---
+++ html-css/exercicios/ex003/js/scripts.js
@@ -0,0 +1,15 @@
+//const {CepBrasil, CorreiosBrasil, RastreioBrasil} =require('correios-brasil')
+/** 
+ * Como Consultar um CEP
+*/
+//cep = new CepBrasil('12345-678')
+
+//cep.consultarCEP().then((Response)=>{
+    //console.log(Response)
+//})
+/**
+ * Como Calcular preços e prazos
+ */
+/**
+ * Como Rastrear uma encomenda
+ */
